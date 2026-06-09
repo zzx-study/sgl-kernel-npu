401ecd71473bf5b9a3c0bb6e7d44c2eab12c77be
zixiangZhu <zhuzixiang2@h-part.com>
2026-06-09 03:19:16 +0000

    debug

--- python/deep_ep/deep_ep/alltoall.py
+++ python/deep_ep/deep_ep/alltoall.py
@@ -365,7 +365,7 @@ def alltoall_low_latency_dispatch(
         recv_x_out = recv_x
 
     packed_recv_count = torch.full(
-        (expert_capacity,), num_local_experts, dtype=torch.int64, device=x.device,
+        (num_local_experts,), expert_capacity, dtype=torch.int64, device=x.device,
     )
 
     handle_tuple = (
